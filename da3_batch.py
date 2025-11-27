16f2c153d6167f86eee9fa9b3e07cc57290cb0dd
oumaima <oumaima@mapersive.com>
2025-11-27 14:12:07 +0100

    Change back to DA3METRIC-LARGE
    the base use intrinsics but does not accept custom ones

--- da3_batch.py
+++ da3_batch.py
@@ -18,10 +18,10 @@ def parse_arguments():
 
 
 def _load_model(device: str):
-    """Load DA3-BASE model."""
+    """Load DA3METRIC-LARGE model."""
     from depth_anything_3.api import DepthAnything3
     #model = DepthAnything3.from_pretrained("depth-anything/DA3METRIC-LARGE").to(device).eval()
-    model = DepthAnything3.from_pretrained("depth-anything/DA3-BASE").to(device).eval()
+    model = DepthAnything3.from_pretrained("depth-anything/DA3METRIC-LARGE").to(device).eval()
     return model
 
 
@@ -179,7 +179,7 @@ def main():
     print(f"[DA3] Batch size: {batch_size}")
 
     # Load model
-    print(f"[DA3] Loading model: DA3-BASE ...")
+    print(f"[DA3] Loading model: DA3METRIC-LARGE ...")
     t_load_start = time.perf_counter()
     model = _load_model(device)
     t_load = time.perf_counter() - t_load_start
